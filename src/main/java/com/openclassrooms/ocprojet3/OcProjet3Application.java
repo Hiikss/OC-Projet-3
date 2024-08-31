4ed68b952d1a90d7a11c306b02f32a6f7d47ed6e
Thomas <91778476+Hiikss@users.noreply.github.com>
2024-08-31 16:25:24 +0200

    First commit

---
+++ src/main/java/com/openclassrooms/ocprojet3/OcProjet3Application.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.ocprojet3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcProjet3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcProjet3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/ocprojet3/OcProjet3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.ocprojet3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcProjet3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
